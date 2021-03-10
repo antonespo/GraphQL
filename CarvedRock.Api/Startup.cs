ad5547a1548f1991af0995e650b44a8bfa9ab017
AntonioE <antonio.esposito4@live.it>
2021-03-10 22:37:28 +0100

    Demo up and running

--- CarvedRock.Api/Startup.cs
+++ CarvedRock.Api/Startup.cs
@@ -5,43 +5,41 @@ using GraphQL;
 using GraphQL.Server;
 using GraphQL.Server.Ui.Playground;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CarvedRock.Api
 {
-    public class Startup
+  public class Startup
+  {
+    private readonly IConfiguration _config;
+    // private readonly IHostingEnvironment _env;
+
+    public Startup(IConfiguration config)
     {
-        private readonly IConfiguration _config;
-        private readonly IHostingEnvironment _env;
-
-        public Startup(IConfiguration config, IHostingEnvironment env)
-        {
-            _config = config;
-            _env = env;
-        }
-
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddMvc();
-            services.AddDbContext<CarvedRockDbContext>(options =>
-                options.UseSqlServer(_config["ConnectionStrings:CarvedRock"]));
-            services.AddScoped<ProductRepository>();
-
-            services.AddScoped<IDependencyResolver>(s => new FuncDependencyResolver(s.GetRequiredService));
-            services.AddScoped<CarvedRockSchema>();
-
-            services.AddGraphQL(o => { o.ExposeExceptions = false; })
-                .AddGraphTypes(ServiceLifetime.Scoped);
-        }
-
-        public void Configure(IApplicationBuilder app, CarvedRockDbContext dbContext)
-        {
-            app.UseGraphQL<CarvedRockSchema>();
-            app.UseGraphQLPlayground(new GraphQLPlaygroundOptions());
-            dbContext.Seed();
-        }
+      _config = config;
     }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddMvc();
+      services.AddDbContext<CarvedRockDbContext>(options =>
+      options.UseSqlServer(_config.GetConnectionString("CarvedRock")));
+      services.AddScoped<ProductRepository>();
+
+      services.AddScoped<IDependencyResolver>(s => new FuncDependencyResolver(s.GetRequiredService));
+      services.AddScoped<CarvedRockSchema>();
+
+      services.AddGraphQL(o => { o.ExposeExceptions = false; })
+          .AddGraphTypes(ServiceLifetime.Scoped);
+    }
+
+    public void Configure(IApplicationBuilder app, CarvedRockDbContext dbContext)
+    {
+      app.UseGraphQL<CarvedRockSchema>();
+      app.UseGraphQLPlayground(new GraphQLPlaygroundOptions());
+      dbContext.Seed();
+    }
+  }
 }
---
+++ GraphQL.Demo.API/Startup.cs
@@ -0,0 +1,67 @@
+﻿using CarvedRock.Api.Data;
+using CarvedRock.Api.GraphQL;
+using CarvedRock.Api.Repositories;
+using GraphQL.Server;
+using GraphQL.Server.Ui.Playground;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace GraphQL.Demo.API
+{
+  public class Startup
+  {
+
+    public Startup(IConfiguration configuration)
+    {
+      Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddControllers();
+
+      services.AddDbContext<CarvedRockDbContext>(options =>
+      options.UseSqlServer(Configuration.GetConnectionString("CarvedRock")));
+      services.AddScoped<ProductRepository>();
+
+      services.AddScoped<IDependencyResolver>(s => new FuncDependencyResolver(s.GetRequiredService));
+      services.AddScoped<CarvedRockSchema>();
+
+      services.AddGraphQL(o => { o.ExposeExceptions = false; })
+          .AddGraphTypes(ServiceLifetime.Scoped);
+
+      services.Configure<KestrelServerOptions>(o => o.AllowSynchronousIO = true);
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      app.UseHttpsRedirection();
+
+      app.UseRouting();
+
+      app.UseAuthorization();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+
+      app.UseGraphQL<CarvedRockSchema>();
+      app.UseGraphQLPlayground(new GraphQLPlaygroundOptions());
+    }
+  }
+}
